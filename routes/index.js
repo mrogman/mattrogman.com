3ad4ef40a6fdd89fc8d4aed1195b0814b50707ce
d30xy <mrogman@gmail.com>
2016-01-15 13:20:24 -0500

    plain under construction view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Matt Rogman (mrogman)' });
 });
 
 module.exports = router;
